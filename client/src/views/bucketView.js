61172c8a7fc15d5da2db02681a34dfcc27ec2c18
David Rawson <dm.rawson@gmail.com>
2018-02-28 19:48:48 +0000

    BucketView started. <UL> put into HMTL.

--- client/src/views/bucketView.js
+++ client/src/views/bucketView.js
@@ -1,3 +1,15 @@
 const BucketView = function(){
   this.bucketView = [];
 }
+
+BucketView.prototype.addItem = function(item){
+  this.bucketView.push(item);
+  this.render(item);
+}
+
+
+BucketView.prototype.clear = function(){
+  this.bucketView = [];
+  const ul = document.querySelector('#bucket-list');
+  ul.innerHTML = '';
+}
